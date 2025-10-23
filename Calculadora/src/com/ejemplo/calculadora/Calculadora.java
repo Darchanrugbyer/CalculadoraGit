d144bba0f338779e53840fbb7ed3e3b7fe5fbed4
darchanrugbyer <dlopher2005@g.educaand.esgit config --global user.email dlopher2005@g.educaand.es>
2025-10-23 12:04:30 +0200

    Añadida operación de división con control de error

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -8,4 +8,9 @@ public class Calculadora {
 	 return a - b;}
     public double multiplicar(double a, double b) {
      return a*b;}
-    }
+    public double dividir(double a, double b) {
+    	 if (b == 0) {
+    	 throw new IllegalArgumentException("No se puede dividir entre cero");
+    	 }
+    	 return a / b;}
+}
